8bf76eb75e1795b7b9d772e59ddcbc84181cdab0
Marc <marcbenet@gmail.com>
2021-01-17 22:20:13 +0100

    Adds local IP address to Telegram reports

--- motion_notification_bot.py
+++ motion_notification_bot.py
@@ -1,12 +1,11 @@
+import logging
 import os
+import socket
 import subprocess
 import time
-import logging
 
 import telegram
-
 from environs import Env
-
 
 env = Env()
 env.read_env()
@@ -16,6 +15,7 @@ bot_token = env("TELEGRAM_BOT_TOKEN")
 chat_id = env("TELEGRAM_CHAT_ID")
 report_interval = env.int("REPORT_INTERVAL")  #  in seconds
 notify_log_path = env("NOTIFIER_LOG_PATH")
+feed_port = env("FEED_PORT", default="8081")
 logging.basicConfig(
     filename=env("NOTIFIER_LOG_PATH"),
     filemode="w",
@@ -32,6 +32,18 @@ last_pic_path = ""
 time_interval = int(time.time())
 initial_time = int(time.time())
 final_time = int(time.time())
+
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+
+    return ip
+
+
+feed_ip = get_local_ip()
 
 
 def cleanup_file(path):
@@ -51,7 +63,7 @@ def get_file_from_log_line(log_line):
 
 def telegram_msg(text):
     bot = telegram.Bot(token=bot_token)
-    bot.send_message(chat_id=chat_id, text=text)
+    bot.send_message(chat_id=chat_id, text=f"http://{feed_ip}:{feed_port} - {text}")
 
 
 def telegram_photo(path):
@@ -71,7 +83,7 @@ def telegram_photo(path):
 def telegram_video(path, rm_file=True):
     if "mp4" in path:
         bot = telegram.Bot(token=bot_token)
-        bot.send_message(chat_id=chat_id, text="Sending recording...")
+        bot.send_message(chat_id=chat_id, text=f"Sending recording...")
         try:
             bot.send_video(chat_id, video=open(path, "rb"))
             if rm_file:
@@ -84,68 +96,69 @@ def telegram_video(path, rm_file=True):
             )
 
 
-telegram_msg("SURVEILANCE STARTED")
-f = subprocess.Popen(
-    ["tail", "-F", filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-)
+if __name__ == "__main__":
+    telegram_msg("SURVEILANCE STARTED")
+    f = subprocess.Popen(
+        ["tail", "-F", filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
 
-while True:
-    try:
-        line = f.stdout.readline().decode()
-        if "File of type 8 saved to:" in line:
-            last_video_path = get_file_from_log_line(line)
-            logging.info(f"NEW VIDEO EVENT LOG - {line}")
-            logging.info(f"FILE PATH - {last_pic_path}")
+    while True:
+        try:
+            line = f.stdout.readline().decode()
+            if "File of type 8 saved to:" in line:
+                last_video_path = get_file_from_log_line(line)
+                logging.info(f"NEW VIDEO EVENT LOG - {line}")
+                logging.info(f"FILE PATH - {last_pic_path}")
 
-        if "motion_detected:" in line:
-            initial_time = int(time.time())
-            logging.info("MOTION DETECTED!")
+            if "motion_detected:" in line:
+                initial_time = int(time.time())
+                logging.info("MOTION DETECTED!")
 
-        if "Thread exiting" in line:
-            logging.info("MOTION PROCESS SHUTDOWN")
-            telegram_msg("Camera shutting down...")
+            if "Thread exiting" in line:
+                logging.info("MOTION PROCESS SHUTDOWN")
+                telegram_msg("Camera shutting down...")
 
-        if "motion_init: Camera 0 started" in line:
-            logging.info("MOTION PROCESS STARTUP")
-            telegram_msg("Camera started")
+            if "motion_init: Camera 0 started" in line:
+                logging.info("MOTION PROCESS STARTUP")
+                telegram_msg("Camera started")
 
-        if "End of event" in line:
-            final_time = int(time.time())
-            logging.info(f"VIDEO PATH - {last_video_path}")
-            if (final_time - initial_time) > 20:
-                logging.info("RELEVANT MOVIE FINISHED!")
-                telegram_photo(last_pic_path)
-                telegram_video(last_video_path)
-                relevant_counter += 1
-                last_relevant = True
-            elif last_relevant:
-                # if it started relevant we want the final of the video even
-                # if it was not going to be considered relevant
-                logging.info("RELEVANT BECAUSE LAST RELEVANT FINISHED!")
-                telegram_photo(last_pic_path)
-                telegram_video(last_video_path)
-                relevant_counter += 1
-                last_relevant = False
-            else:
-                irrelevant_counter += 1
-                cleanup_file(last_pic_path)
-                cleanup_file(last_video_path)
-                logging.info("IRRELEVANT MOVIE")
-                last_relevant = False
+            if "End of event" in line:
+                final_time = int(time.time())
+                logging.info(f"VIDEO PATH - {last_video_path}")
+                if (final_time - initial_time) > 30:
+                    logging.info("RELEVANT MOVIE FINISHED!")
+                    telegram_photo(last_pic_path)
+                    telegram_video(last_video_path)
+                    relevant_counter += 1
+                    last_relevant = True
+                elif last_relevant:
+                    # if it started relevant we want the final of the video even
+                    # if it was not going to be considered relevant
+                    logging.info("RELEVANT BECAUSE LAST RELEVANT FINISHED!")
+                    telegram_photo(last_pic_path)
+                    telegram_video(last_video_path)
+                    relevant_counter += 1
+                    last_relevant = False
+                else:
+                    irrelevant_counter += 1
+                    cleanup_file(last_pic_path)
+                    cleanup_file(last_video_path)
+                    logging.info("IRRELEVANT MOVIE")
+                    last_relevant = False
 
-        if "File of type 1 saved to:" in line:
-            last_pic_path = get_file_from_log_line(line)
-            logging.info(f"NEW IMAGE EVENT LOG - {line}")
-            logging.info(f"FILE PATH - {last_pic_path}")
+            if "File of type 1 saved to:" in line:
+                last_pic_path = get_file_from_log_line(line)
+                logging.info(f"NEW IMAGE EVENT LOG - {line}")
+                logging.info(f"FILE PATH - {last_pic_path}")
 
-        if (int(time.time()) - time_interval) > report_interval:
-            msg = "---- REPORT ---- \n"
-            msg += f"-> {irrelevant_counter} irrelevant events \n"
-            msg += f"-> {relevant_counter} videos recorded"
-            telegram_msg(msg)
-            time_interval = int(time.time())
-            irrelevant_counter = 0
-            relevant_counter = 0
+            if (int(time.time()) - time_interval) > report_interval:
+                msg = "---- REPORT ---- \n"
+                msg += f"-> {irrelevant_counter} irrelevant events \n"
+                msg += f"-> {relevant_counter} videos recorded"
+                telegram_msg(msg)
+                time_interval = int(time.time())
+                irrelevant_counter = 0
+                relevant_counter = 0
 
-    except telegram.error.TimedOut:
-        logging.critical("Telegram TimeOut Error :(")
+        except telegram.error.TimedOut:
+            logging.critical("Telegram TimeOut Error :(")
